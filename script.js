78e747ba2270f8a65bdc863067929ccfb9b2227b
Start Coding Student 2 <session2@START-Coding-MBP-08.local>
2016-01-06 18:10:28 -0700

    set new variables made bar for health and stamina

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 /**
  * Created by Erick on 12/27/2015.
  */
+function startGame(){
+    confirm('you did it');
+}
+
+function Pic(src) {
+    var img = document.createElement("img");
+    img.src = src;
+    img.width = 600;
+    img.height = 300;
+    img.id = 'picture';
+// This next line will just add it to the <body> tag
+    document.getElementById('imageBox').appendChild(img);
+}
+
+var topHealth = 5;
+var botHealth = 5;
+var topStam = 5;
+var botStam = 5;
+
