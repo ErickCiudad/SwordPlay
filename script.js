7d10cd6874e6055183ff429e6de247e35fa414ca
Start Coding Student 2 <session2@START-Coding-MBP-08.local>
2016-01-08 18:09:14 -0700

    Great day! Stamina stops when you are attacking, you die if your health is zero, game restarts.

--- script.js
+++ script.js
@@ -17,32 +17,64 @@ function Pic(src) {
 
 var topHealth = 5;
 var botHealth = 5;
+
 var topStam = 5;
+var topStamPS = 5;
+
 var botStam = 5;
 var botStamPS = .1;
 
+var topStatus = 'resting';
+var botStatus = 'resting';
 
 
 function autoClick(){
-    botStam+=botStamPS;
+    if (botStatus === 'resting'){
+    botStam+=botStamPS;}
+    Math.floor(botStam);
     document.getElementById("botStam").innerHTML= botStam.toFixed(0);
     if (botStam >= 5) {
         //document.getElementById("botStam").innerHTML= 5;
         botStam = 5;
     }
+    document.getElementById("botHealth").innerHTML= botHealth.toFixed(0);
+    document.getElementById("topHealth").innerHTML= topHealth.toFixed(0);
 }
+
 setInterval(autoClick, 100);
 //this runs 10 times per second
 
-document.getElementById("botStam").innerHTML= botStam.toFixed(0);
 
 function botAttack() {
-    if (botStam <= 1){
+    if (botStam <= 0){
         alert('out');
-        return;
     }
-    botStam-=1;
+    else {
+        botStatus = 'attacking';
+        setTimeout(botRest, 200);
+        function botRest (){
+            botStatus = 'resting'
+        }
+        botStam -= 1;
+        topHealth -= 1;
+        if (topHealth <= 0) {
+            setTimeout(topDied, 100);
+            function topDied() {
+                alert('top player died');
+                topHealth = 5;
+                botStam = 5;
+            }
+        }
+    }
 }
+
+/*attack doesn't run if stamina is empty
+* if your attacking, you're status is set and stamina recovery is stopped
+* if you have attacked, you lose one stamina point and your enemy loses one health point.
+* if your attack made a kill, it states it and restarts the game.
+* */
+
 
 //functions in global only run on load
 
+//    document.getElementById("botStam").innerHTML= botStam.toFixed(0);
